0b18d889fd5a3e0fc889667de716ce202a42a61f
Michael Harris <mhguitar1997@gmail.com>
2019-07-16 12:58:24 -0700

    Dark mode values and set value for dark mode using custom hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,4 +1,4 @@
 import { useState } from "react";
 import {useLocalStorage} from './useLocalStorage'
 
-export const useDarkMode = () => {};
+const [darkMode, setDarkMode] = useLocalStorage("darkMode-On")
