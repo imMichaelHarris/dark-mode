d26a9eac8a9eba77e22151cc42fcf324f4b95784
Michael Harris <mhguitar1997@gmail.com>
2019-07-16 13:12:40 -0700

    useDarkMode returns value and setter function

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,4 +1,16 @@
 import { useState, useEffect } from "react";
-import {useLocalStorage} from './useLocalStorage'
+import { useLocalStorage } from "./useLocalStorage";
 
-const [darkMode, setDarkMode] = useLocalStorage("darkMode-On")
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode-On");
+
+  useEffect(() => {
+    if (darkMode) {
+      document.querySelector("body").classList.add("dark-mode");
+    } else {
+      document.querySelector("body").classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  return [darkMode, setDarkMode];
+};
